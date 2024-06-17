044e0f76707faaa37857459d68bcb4586fe3a3a5
Mattia018 <mattiapresta15@gmail.com>
2024-06-17 17:20:56 +0200

    appost

--- base/__init__.py
+++ base/__init__.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+import os
+
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/app_db'
+
+
 app.config["SQLALCHEMY_DATABASE_URI"]= 'mysql+pymysql://root:@34.32.36.123/appdb'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
